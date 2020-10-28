69167c3ff2f61d14664887db0cb05ad4018b6903
Florian Milcendeau <fmilcendeau85@gmail.com>
2020-10-28 11:12:33 +0100

    initialisation du composant et appelle de l'api bicloo

---
+++ src/components/WrapperStation.jsx
@@ -0,0 +1,18 @@
+import Axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+const WrapperStation = () => {
+  const [dataStation, setdataStation] = useState(null);
+
+  useEffect(() => {
+    Axios.get(
+      'https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_stations-velos-libre-service-nantes-metropole-disponibilites&q=&rows=10&facet=banking&facet=bonus&facet=status&facet=contract_name'
+    ).then((res) => {
+      setdataStation(res.data);
+    });
+  }, []);
+
+  return <main></main>;
+};
+
+export default WrapperStation;
