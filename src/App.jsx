5fd5f7baf12b5433b6bc83a5d41e9590adbce229
Florian Milcendeau <fmilcendeau85@gmail.com>
2020-10-28 11:15:12 +0100

    composant 'WrapperStation' importer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import WrapperStation from './components/WrapperStation';
 import styles from './css/app.module.css';
 
 function App() {
-  return <div className={styles.app}>Almost Hello World!</div>;
+  return (
+    <div className={styles.app}>
+      <WrapperStation />
+    </div>
+  );
 }
 
 export default App;
