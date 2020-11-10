1c291cf497d9391bfc455a160b51ab223b358f3d
nicolas <nicolas.mercier570@gmail.com>
2020-11-10 14:23:57 +0100

    Removed comment

--- src/components/StationsList.jsx
+++ src/components/StationsList.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import CardList from './CardList';
 
-// import { scroll } from './ListSlider';
-
 import styles from '../css/stationsList.module.css';
 
 const StationsList = ({
