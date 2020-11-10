4042be5a9f0801e202774e40ce4ba9fffdca3427
nicolas <nicolas.mercier570@gmail.com>
2020-11-10 11:00:35 +0100

    making slider scrollable

--- src/components/ListSlider.jsx
+++ src/components/ListSlider.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createRef, useEffect, useCallback } from 'react';
 import Slider from 'react-slick';
 import PropTypes from 'prop-types';
 
@@ -7,19 +7,47 @@ import CardList from './CardList';
 import styles from '../css/listSlider.module.css';
 
 function ListSlider({ stations }) {
+  const sliderRef = createRef();
+  const scroll = useCallback(
+    (y) => {
+      if (y > 0) {
+        return sliderRef.current.slickNext();
+      }
+      return sliderRef.current.slickPrev();
+    },
+    [sliderRef]
+  );
+
+  useEffect(() => {
+    window.addEventListener('wheel', (e) => {
+      scroll(e.deltaY);
+    });
+  });
+
   const settings = {
     infinite: false,
     speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 2,
+    slidesToShow: 3,
     vertical: true,
     verticalSwiping: true,
     swipeToSlide: true,
+    draggable: true,
   };
+
+  // const slide = (y) => {
+  //   const { current } = slider;
+  //   return y > 0 ? current.slickNext() : current.slickPrev();
+  // };
+
+  // useEffect(() => {
+  //   window.addEventListener('wheel', (e) => {
+  //     slide(e.wheelDelta);
+  //   });
+  // }, []);
 
   return (
     <div className={styles.container}>
-      <Slider {...settings}>
+      <Slider ref={sliderRef} {...settings} className={styles.slider}>
         {stations.map((station) => {
           return <CardList key={station.id} {...station} />;
         })}
