1dd939436a0147aad4658c37038d148c3918fcbe
Florian Milcendeau <fmilcendeau85@gmail.com>
2020-11-27 13:00:14 +0100

    scroll ajouter sur la liste

--- src/components/ListSlider.jsx
+++ src/components/ListSlider.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import SwiperCore, { Scrollbar, Mousewheel } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
 import PropTypes from 'prop-types';
 
 import CardList from './CardList';
-
 import '../css/listSlider.css';
-import 'swiper/components/scrollbar/scrollbar.scss';
-
-SwiperCore.use([Scrollbar, Mousewheel]);
 
 function ListSlider({
   bikesIsChecked,
@@ -20,45 +14,35 @@ function ListSlider({
 }) {
   return (
     <div className="container">
-      <Swiper
-        className="slider"
-        direction="vertical"
-        mousewheel
-        scrollbar={{ draggable: true }}
-        spaceBetween={50}
-        slidesPerView={3}
-      >
-        {stations
-          .filter((station) => {
-            if (bankingIsChecked) {
-              return station.banking === 'True';
-            }
-            return station;
-          })
-          .filter((station) => {
-            if (bikesIsChecked) {
-              return station.availableBikes > 0;
-            }
-            return station;
-          })
-          .filter((station) => {
-            if (standsIsChecked) {
-              return station.availableBikeStand > 0;
-            }
-            return station;
-          })
-          .map((station) => {
-            return (
-              <SwiperSlide key={station.id}>
-                <CardList
-                  station={station}
-                  handleRoutingControl={handleRoutingControl}
-                  display={display}
-                />
-              </SwiperSlide>
-            );
-          })}
-      </Swiper>
+      {stations
+        .filter((station) => {
+          if (bankingIsChecked) {
+            return station.banking === 'True';
+          }
+          return station;
+        })
+        .filter((station) => {
+          if (bikesIsChecked) {
+            return station.availableBikes > 0;
+          }
+          return station;
+        })
+        .filter((station) => {
+          if (standsIsChecked) {
+            return station.availableBikeStand > 0;
+          }
+          return station;
+        })
+        .map((station) => {
+          return (
+            <CardList
+              key={station.id}
+              station={station}
+              handleRoutingControl={handleRoutingControl}
+              display={display}
+            />
+          );
+        })}
     </div>
   );
 }
